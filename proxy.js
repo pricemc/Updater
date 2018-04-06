7b96669b7d1ecf5f036d25c504ab55ec2367f8f8
Matthew Curtis Price <mcp97@byu.local>
2018-04-06 15:24:03 -0600

    dev

--- proxy.js
+++ proxy.js
@@ -1,4 +1,3 @@
 var proxy = require('redbird')({port: 8000});
 
-proxy.register("localhost", "localhost:3011");
-proxy.register("update.localhost", "localhost:3011");
+proxy.register("update.mattcprice.com", "localhost:3011");
